077f42ef84c83605aedcc02bf0d882d5b2335655
LAPTOP-VKH4P47T\DASH F15 <ajunquit@gmail.com>
2024-01-02 17:18:53 -0500

    Add Singleton Design Pattern with the example Print Spooler.

---
+++ Singleton.PrintSpooler/PrintSpoolerThreadSafe.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton.PrintSpooler;
+
+public class PrintSpoolerThreadSafe
+{
+    private static volatile PrintSpoolerThreadSafe instance;
+    private static readonly object syncRoot = new object();
+
+    public PrintSpoolerThreadSafe()
+    {
+        
+    }
+
+    public static PrintSpoolerThreadSafe GetInstance()
+    {
+        if (instance is null)
+        {
+            lock (syncRoot)
+            {
+                if(instance is null)
+                {
+                    instance = new PrintSpoolerThreadSafe();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+    public void Print(string document)
+    {
+        Console.WriteLine($"Printing: {document}");
+    }
+}
---
+++ Singleton.PrintSpooler/Program.cs
@@ -0,0 +1,10 @@
+﻿using Singleton.PrintSpooler;
+
+await Task.Run(() => PrintWithThreadSafeSpooler("document 1"));
+await Task.Run(() => PrintWithThreadSafeSpooler("document 2"));
+
+static void PrintWithThreadSafeSpooler(string document)
+{
+    PrintSpoolerThreadSafe spooler = new PrintSpoolerThreadSafe();
+    spooler.Print(document);
+}
