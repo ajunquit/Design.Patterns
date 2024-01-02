19b896c531d9803e3d31f05ab6b64a69ffbbae65
Alejandro Junqui <ajunquit@gmail.com>
2024-01-02 18:36:33 -0500

    Add example design pattern singleton

---
+++ Singleton.ConnectionString/DatabaseConnection.cs
@@ -0,0 +1,34 @@
+﻿namespace Singleton.ConnectionString;
+
+public class DatabaseConnection
+{
+    private string Server { get; set; }
+    private string Database { get; set; }
+    private string UserId { get; set; }
+    private string Password { get; set; }
+
+    private static DatabaseConnection _connectionDatabase;
+
+    private DatabaseConnection()
+    {
+        Server = "myServer";
+        Database = "myDatabase";
+        UserId = "myUserId";
+        Password = "myPassword";
+    }
+
+    public string GetConnectionString()
+    {
+        return $"Server={Server};Database={Database};User Id={UserId};Password={Password};";
+    }
+
+    public static DatabaseConnection GetConnectionDatabase()
+    {
+        if (_connectionDatabase == null)
+        {
+            _connectionDatabase = new DatabaseConnection();
+        }
+        return _connectionDatabase;
+    }
+
+}
--- Singleton.ConnectionString/Program.cs
+++ Singleton.ConnectionString/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Singleton.ConnectionString;
+
+
+// get connection for the first time.
+DatabaseConnection connectionSample1 = DatabaseConnection.GetConnectionDatabase();
+Console.WriteLine(connectionSample1.GetConnectionString());
+
+// get connection for the second time.
+DatabaseConnection connectionSample2 = DatabaseConnection.GetConnectionDatabase();
+Console.WriteLine(connectionSample2.GetConnectionString());
+
+Console.ReadLine();
