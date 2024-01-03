c42b2665b264efce4f6323167f4840f74b3f5284
Alejandro Junqui <ajunquit@gmail.com>
2024-01-02 19:44:37 -0500

    Add document example of factory method design pattern.

---
+++ FactoryMethod.Document/FactoryMethodDocument.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod.Document;
+
+// product
+public interface IDocument
+{
+    void Open();
+    void Close();
+}
+
+// concrete product - pdf document
+public class PdfDocument : IDocument
+{
+    public void Close()
+    {
+        Console.WriteLine("Saving PDF document.");
+    }
+
+    public void Open()
+    {
+        Console.WriteLine("Opening PDF document.");
+    }
+}
+
+// concrete product - word document
+public class WordDocument : IDocument
+{
+    public void Close()
+    {
+        Console.WriteLine("Saving Word document.");
+    }
+
+    public void Open()
+    {
+        Console.WriteLine("Opening Word document.");
+    }
+}
+
+// creator interface
+public interface IDocumentConverter
+{
+    IDocument CreateDocument();
+}
+
+// concrete creator for pdf documents
+
+public class PdfDocumentConverter : IDocumentConverter
+{
+    public IDocument CreateDocument()
+    {
+        return new PdfDocument();
+    }
+}
+
+// concrete creator for word documents 
+public class WordDocumentConverter : IDocumentConverter
+{
+    public IDocument CreateDocument()
+    {
+        return new WordDocument();
+    }
+}
---
+++ FactoryMethod.Document/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using FactoryMethod.Document;
+// Using PdfDocumentConverter to create and manipulate PDF document
+IDocumentConverter pdfConverter = new PdfDocumentConverter();
+IDocument pdfDocument = pdfConverter.CreateDocument();
+pdfDocument.Open();
+pdfDocument.Close();
+
+// Using WordDocumentConverter to create and manipulate Word document
+IDocumentConverter wordConverter = new WordDocumentConverter();
+IDocument wordDocument = wordConverter.CreateDocument();
+wordDocument.Open();
+wordDocument.Close();
+    
